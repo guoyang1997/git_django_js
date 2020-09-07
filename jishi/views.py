20a3ea4e9110d37a9878864760a0dacd3cbfd7f8
guoyang1997 <70315715+guoyang1997@users.noreply.github.com>
2020-09-07 11:21:20 +0800

    Update views.py

--- jishi/views.py
+++ jishi/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def add(request):
+    a = 1
+    b = 2
+    return HttpResponse(a+b)
+
+
 def dev2(request):
     return HttpResponse("OK")
